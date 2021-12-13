3ec453fcc715e66cb9bfdc24deda1a67926787a5
Gilang Adam <gilangbram@gmail.com>
2021-12-13 17:33:10 +0700

    Mutation resolvers (#3)
    * :zap: add resolver for Mutation.login
    
    * :zap: add resolvers for Mutation.bookTrips & Mutation.cancelTrip
    
    * :zap: test mutations
    
    * :sparkles: update .gitignore

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const isEmail = require('isemail');
 const { ApolloServer } = require('apollo-server');
 const { createStore } = require('./utils');
 const typeDefs = require('./schema');
@@ -8,11 +9,30 @@ const resolvers = require('./resolvers');
 const LaunchAPI = require('./datasources/launch');
 const userAPI = require('./datasources/user');
 
-// function to set up our SQLite database
-const store = createStore();
+const store = createStore(); // function to set up our SQLite database
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
+    /**
+     * The context function defined below is called once for every GraphQL operation
+     * that clients send to our server.
+     * The return value of this function becomes the context argument
+     * that's passed to every resolver that runs as part of that operation.
+     */
+    context: async ({ req }) => {
+      /**
+       * Simple auth check on every request
+       * The authentication method used in this example application is not at all secure and should not be used by production applications.
+       * However, you can apply the principles demonstrated below to a token-based authentication method that is secure.
+       */
+      const auth = (req.headers && req.headers.authorization) || ''; // get the auth header (if present)
+      const email = Buffer.from(auth, 'base64').toString('ascii'); // decode the value of the authorization header from base64
+      if (!isEmail.validate(email)) return { user: null }; // if not a valid email, return null for user
+      // find a user by their email
+      const users = await store.users.findOrCreate({ where: { email } }); // find or create a user with that email
+      const user = (users && users[0]) || null; // get the user (if one was found)
+      return { user: { ...user.dataValues } }; // add the user to the context
+    },
     // cors: true,
     typeDefs,
     resolvers,
